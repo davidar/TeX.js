e5aadaef68a0a299fd30ffe043b2baee9927a60e
David A Roberts <d@vidr.cc>
2016-02-28 14:04:24 +1000

    load.js

---
+++ load.js
@@ -0,0 +1,8 @@
+var s = document.createElement('script');
+s.setAttribute('data-main', '//davidar.io/TeX.js/main');
+s.setAttribute('src', '//cdnjs.cloudflare.com/ajax/libs/require.js/2.1.20/require.min.js');
+if(document.body !== null) {
+    document.body.appendChild(s);
+} else {
+    document.head.appendChild(s);
+}
